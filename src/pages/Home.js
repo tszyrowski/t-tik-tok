c5a70106ed2f5b2211bb2441d3abaaca48982fde
tomasz szyrowski <tszyrowski@gmail.com>
2021-10-13 19:35:19 +0000

    main: change Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,92 +1,24 @@
-import React, { useState, useEffect } from 'react'
 import FollowersColumn from '../components/FollowersColumn'
-import Card from '../components/Card'
-import MiniCard from '../components/MiniCard'
-import axios from 'axios'
 
 const Home = () => {
-  const [users, setUsers] = useState(null)
-  const [userToToggle, setUserToToggle] = useState(null)
-  let descendingUsers
-  let topFiveNotFollowing
-  let topFiveFollowing
-
-  //auto populating with dummy data
-  const addData = async () => {
-    await axios.post('/.netlify/functions/addData')
-  }
-
-  //fetch all the tik-tok posts to your feed
-  const fetchData = async () => {
-    const results = await axios.get('/.netlify/functions/post')
-    console.log(results.data)
-    setUsers(results.data)
-  }
-
-  //toggle user from followed to unfollowed
-  if (userToToggle) {
-  const newValue = userToToggle.is_followed ? false : true
-  const data = {is_followed: newValue}
-
-  console.log(newValue)
-  console.log(userToToggle.id)
-
-   axios.put('/.netlify/functions/edit', {userId: userToToggle.id, data: data })
-    .then(json => console.log(json))
-    .catch(err => console.error('error:' + err))
-    .then(() => fetchData())
-      setUserToToggle(null)
-  }
-
-  useEffect(() => {
-    addData()
-    fetchData()
-  }, [])
-
-  console.log(users)
-
-  if (users) {
-    console.log(users)
-    descendingUsers = users.sort((a, b) => a.id < b.id ? 1 : -1)
-    const following = users.filter(user => user.is_followed === true)
-    const descendingFollowing = following.sort((a, b) => a.likes < b.likes ? 1 : -1)
-    topFiveFollowing = descendingFollowing.slice(0, 5)
-
-    const notFollowing = users.filter((user) => user.is_followed === false)
-    const descendingNotFollowing = notFollowing.sort((a, b) => a.likes < b.likes ? 1 : -1)
-    topFiveNotFollowing = descendingNotFollowing.slice(0, 5)
-  }
 
   return (
     <>
-    {descendingUsers && (
       <div className='container'>
-        <FollowersColumn users={topFiveFollowing} />
+        <FollowersColumn />
         <div className='feed'>
-          {descendingUsers.map((descendingUser, index) => (
-            <Card
-              key={index}
-              user={descendingUser}
-              toggleFollow={userToToggle => setUserToToggle(userToToggle)}
-            />
-          ))}
+  
         </div>
         <div className="suggested-box">
           <div className="section">
             <div className="suggested">
               <h2 className="bold">Suggested accounts</h2>
               <div className="break" />
-              {topFiveNotFollowing.map((notFollowingUser, index) => (
-                <MiniCard 
-                  key={index} user={notFollowingUser}
-                  toggleFollow={userToToggle => setUserToToggle(userToToggle)}
-		  	        />)
-		  	      )}
+
             </div>
           </div>
         </div>
       </div>
-      )}
     </>
   )
 }
